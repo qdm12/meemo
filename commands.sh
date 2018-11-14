71c32a81a23d387e7381bdf34c01e82673dc1218
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-11-14 16:20:47 +0200

    Added sudo to docker exec commands

--- commands.sh
+++ commands.sh
@@ -7,7 +7,7 @@ fi
 case "$1" in
   "ls")
     echo "====== List of users ======"
-    docker-compose exec meemo ./meemo/admin users;;
+    sudo docker-compose exec meemo ./meemo/admin users;;
   "add")
     echo "====== Adding user ======"
     USER="$2"
@@ -17,7 +17,7 @@ case "$1" in
     esac
     PASSWORD="$3"
     echo "Adding user $USER with password $PASSWORD"
-    docker-compose exec meemo ./meemo/admin user-add -u $USER -p $PASSWORD --display-name $USER;;
+    sudo docker-compose exec meemo ./meemo/admin user-add -u $USER -p $PASSWORD --display-name $USER;;
   "edit")
     echo "====== Editing user ======"
     USER="$2"
@@ -28,7 +28,7 @@ case "$1" in
     # docker-compose exec meemo ./admin users | grep $USER
     PASSWORD="$3"
     echo "Editing user $USER with password $PASSWORD"
-    docker-compose exec meemo ./meemo/admin user-edit -u $USER -p $PASSWORD --display-name $USER;;
+    sudo docker-compose exec meemo ./meemo/admin user-edit -u $USER -p $PASSWORD --display-name $USER;;
   "remove")
     echo "====== Removing user ======"
     USER="$2"
@@ -36,6 +36,6 @@ case "$1" in
       "") echo "Usage: sudo $0 $1 username" >&2
         exit 1;;
     esac
-    docker-compose exec meemo ./meemo/admin user-del -u $USER;;
+    sudo docker-compose exec meemo ./meemo/admin user-del -u $USER;;
   *) echo "Usage: sudo $0 ls|add|edit|remove"
 esac
